51d2240dc886fcf742e9abcaf5ddc78b3d9ddf15
Sabin Ghimire <sabinghimire223@gmail.com>
2025-07-17 18:52:22 +0545

    Task adding Working

--- script.js
+++ script.js
@@ -1,2 +1,77 @@
+//Varaibles
 const form = document.querySelector(".form");
-const itemList= document.querySelector(".bottomWrapper")
+const itemList = document.querySelector(".bottomWrapper");
+const clr = document.querySelector(".clearBtn");
+const tasks = document.querySelector(".AddTask");
+let text;
+let items = [];
+
+//Functions
+function addItems(e) {
+  e.preventDefault();
+  const input = tasks.value;
+  const data = {
+    text,
+    completed: false,
+  };
+  data.text = input;
+
+  items.push(data);
+  applyitems(items, itemList);
+  //   console.log(data);
+  this.reset();
+}
+
+function applyitems(datas = [], datalist) {
+  datalist.innerHTML = datas
+    .map((item, index) => {
+      return `
+    <div class="tasks">
+         <span><input type="checkbox" class="pending" data-index=${index} name="Task${index}" id="Task${index}" ${
+        item.completed ? "checked" : ""
+      }> <label class="taskLabel pending" for="Task${index}">${
+        item.text
+      }</label></checkbox> </span>
+         <i class="fa-solid fa-trash accent " data-index=${index}></i>
+      </div> `;
+    })
+    .join("");
+  const deleteBtn = document.querySelectorAll(".fa-trash");
+  deleteBtn.forEach((del) => {
+    del.addEventListener("click", deleteData);
+  });
+  const spans = document.querySelectorAll("span");
+  spans.forEach((span) => span.addEventListener("click", toggleClass));
+}
+function toggleStaus(e) {
+  if (!e.target.matches("input")) return;
+  const index = e.target.dataset.index;
+  items[index].completed = !items[index].completed;
+}
+
+function toggleClass(e) {
+  if (!e.target.matches("label")) return;
+  let classList = e.target.classList;
+  if (classList.contains("completed")) {
+    e.target.classList.remove("completed");
+    e.target.classList.add("pending");
+  } else if (classList.contains("pending")) {
+    e.target.classList.remove("pending");
+    e.target.classList.add("completed");
+  }
+}
+function clearALl() {
+  items.splice(0, items.length);
+  //   localStorage.clear("Item");
+  applyitems(items, itemList);
+}
+
+function deleteData(e) {
+  const index = e.target.dataset.index;
+  items.splice(index, 1);
+  applyitems(items, itemList);
+}
+//EventLisnteners
+form.addEventListener("submit", addItems);
+clr.addEventListener("click", clearALl);
+itemList.addEventListener("click", toggleStaus);
